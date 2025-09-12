587c6fe4b7fadf37b1ea4e700e730c295efa483c
Juan-Scocozza <scocozzajuan9@gmail.com>
2025-09-12 20:02:16 -0300

    Se agrega ejercicio 27 y 28 de el practico 2

--- Practico 2/Ejercicio 27.js
+++ Practico 2/Ejercicio 27.js
@@ -1,25 +1,34 @@
-document.querySelector("#btnIngrsar").addEventListener("click", fCalcular)
-let numeroAdivinar 
+document.querySelector("#btnGuardar").addEventListener("click", fCalcular)
+let numero1
 let intentos = 0
 function fCalcular (){
-    let numero = Number(document.querySelector("#txtNumero").value)
-    intentos++
-    let diferencia 
-    if (numero === numeroAdivinar){
-        document.querySelector("#pResultado").innerHTML= "GANASTE" + intentos
-
-    }else{
-         diferencia = numeroAdivinar-numero 
-    } 
     
-if (diferencia > 15) {
-  mensaje = "Estás lejos.";
-} else if (diferencia >= 10 && diferencia <= 15) {
-  mensaje = "Te estás acercando.";
-} else if (diferencia >= 5 && diferencia <= 9) {
-  mensaje = "Cada vez más cerca.";
-} else if (diferencia >= 1 && diferencia <= 4) {
-  mensaje = "Muy pero muy cerca.";
+  
+ numero1 =Number(document.querySelector("#txtNumero1").value)
 }
-document.querySelector("#pResultado").innerHTML = mensaje + " Intentos: " + intentos;
+
+document.querySelector("#btnAdivinar").addEventListener("click", fAdivinar)
+
+function fAdivinar(){
+let numero2 = Number(document.querySelector("#txtNumero2").value)
+intentos ++
+
+if (numero2 === numero1){
+    
+        document.querySelector("#pResultado").innerHTML = "¡Ganaste en " + intentos + " intento!"
+    } else {
+ 
+        let diferencia;
+        if (numero2 > numero1) diferencia = numero2 - numero1
+        else diferencia = numero1 - numero2
+
+        let mensaje = ""
+        if (diferencia > 15) mensaje = "Estás lejos."
+        else if (diferencia >= 10) mensaje = "Te estás acercando."
+        else if (diferencia >= 5) mensaje = "Cada vez más cerca."
+        else if (diferencia >= 1) mensaje = "Muy pero muy cerca."
+
+        document.querySelector("#pResultado").innerHTML = mensaje + " Intentos: " + intentos
+    }
+
 }
---
+++ Practico 2/Ejercicio 28.js
@@ -0,0 +1,25 @@
+document.querySelector("#btnEquipo1").addEventListener("click", fCalcular)
+
+let puntajeEquipo1 = 0
+function fCalcular(){
+    puntajeEquipo1++
+document.querySelector("#pResultadoE1").innerHTML = puntajeEquipo1
+
+if (puntajeEquipo1 >=30){
+    document.querySelector("#btnEquipo1").disabled = true;
+}
+
+}
+
+document.querySelector("#btnEquipo2").addEventListener("click", fCalcular2)
+let puntajeEquipo2 = 0
+
+function fCalcular2(){
+    puntajeEquipo2++
+    document.querySelector("#pResultadoE2").innerHTML = puntajeEquipo2
+if (puntajeEquipo2 >= 30){
+    document.querySelector("#btnEquipo2").disabled = true;
+}
+
+document.querySelector("#pResultadoE2").innerHTML = puntajeEquipo2
+}
